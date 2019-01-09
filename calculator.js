a30317d78581109b45614bf88696ff83d9e05b37
tabinda-s <tabindanajam@gmail.com>
2019-01-09 00:35:53 -0500

    called multiply

--- calculator.js
+++ calculator.js
@@ -13,3 +13,8 @@ function subtract(a,b) {
 function divide(a, b) {
 	return a/b; 
 }
+
+console.log("I'm going use the calculator functions to multiply 5 and 6")
+var x = multiply(5,6)
+
+console.log(x)
