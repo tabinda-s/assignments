74ae9e14ed9f210519be8d7c26ced580f70ae439
tabinda-s <tabindanajam@gmail.com>
2019-01-09 00:27:50 -0500

    added square cube functions and square_n_times

--- calculator.py
+++ calculator.py
@@ -10,7 +10,23 @@ def multiply(a,b):
 def divide(a,b):
 	return a/b
 
+def square(a):
+	return a*a
+
+def cube(a):
+	return a*a*a
+
+def square_n_times(a,n):
+	final_product = a
+	for i in range(0,n):
+		final_product = square(final_product)
+	return	final_product 
+
 print("I'm going use the calculator functions to multiply 5 and 6")
 x = multiply(5,6)
 
 print(x)
+
+y = square_n_times(2,2)
+
+print(y)
